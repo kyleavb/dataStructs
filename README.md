80ba8e797da1c232b7af9e60c22f84ec11f08a69
Kyle Van Bergen <kyleavb@gmail.com>
2019-11-06 16:32:55 -0800

    created basic linked list imp

---
+++ README.md
@@ -0,0 +1,1 @@
+# Practicing Writing Data Structures
---
+++ linkedList.js
@@ -0,0 +1,91 @@
+class Node {
+    constructor( data ){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor( val ){
+        this.head = new Node( val );
+        this.tail = this.head;
+        this.size = 1;
+    }
+
+    add( val ){
+        let insert = new Node( val );
+
+        if( this.size <= 1 ){
+            this.head.next = insert;
+            this.tail = insert;
+        }else{
+            let current = this.head;
+            while( current.next ){
+                current = current.next;
+            }
+            current.next = insert;
+        }
+
+        this.size ++;
+    }
+
+    print( ){
+        let current = this.head;
+        let str = '';
+
+        while( current ){
+            str += ` ${ current.data } `
+            current = current.next;
+        }
+
+        console.log( str );
+    }
+
+    sum( ){
+        let current = this.head;
+        let total = 0;
+
+        while( current ){
+            total += current.data;
+            current = current.next;
+        }
+
+        console.log( total );
+    }
+
+    remove( val ){
+        let current = this.head;
+        if( val ){
+            let previous;
+
+            while( current.data != val ){
+                previous = current;
+                current = current.next;
+            }
+
+            if( previous ){
+                previous.next = current.next
+            }else{
+                this.head = current.next;
+            }
+
+        }else{
+            let previous
+
+            while( current.next != null ){
+                previous = current;
+                current = current.next;
+            }
+            previous.next = null;
+        }
+    }
+}
+
+let temp = new LinkedList( 1 );
+temp.add( 2 );
+temp.add( 3 );
+temp.add( 4 );
+temp.add( 5 );
+temp.add( 6 );
+temp.remove(5);
+temp.print()
